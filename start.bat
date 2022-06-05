974c6a00447ae4290523a183951b885800fb91c2
AnLim <48682228+DevAnlim@users.noreply.github.com>
2022-06-05 21:03:43 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,22 +1,20 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By DecPro" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrators DecPro@ /add >nul
-net localgroup administrators administrators /add >nul
-net user administrators /active:yes >nul
-net user installer /delete
-diskperf -Y >nul
-sc config Audiosrv start= auto >nul
-sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrators:F >nul
-ICACLS C:\Windows\installer /grant administrators:F >nul
-echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again! 
+net user administrator S@ad0.m2 /add >nul
+net localgroup administrators administrator /add >nul
+echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrators
-echo Password: DecPro@
-echo .
-echo  RDP By DecPro
-echo   Connect to Your RDP !
-#ping -n 10 127.0.0.1 >nul
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+echo User: Administrator
+echo Pass: S@ad0.m2
+curl -O https://raw.githubusercontent.com/docefio/Windows2019RDP-Github/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/docefio/Windows2019RDP-Github/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+diskperf -Y >nul
+sc start audiosrv >nul
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+ping -n 10 127.0.0.1 >nul
